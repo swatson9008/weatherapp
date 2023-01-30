31b90638673bc8fb7e169541a751df36892b7de3
Summer <swatson9008@yahoo.com>
2023-01-29 19:40:26 -0500

    changing code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable new-cap */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable quotes */
 /* eslint-disable no-console */
@@ -7,6 +9,17 @@ import './style.css';
 
 const searchField = document.getElementById('searchField');
 const searchB = document.getElementById('searchB');
+const weatherContainer = [];
+
+class weatherC {
+  constructor(temp, feelsLike, weatherDesc, windSpeed, name) {
+    this.name = name;
+    this.temp = temp;
+    this.feelsLike = feelsLike;
+    this.windSpeed = windSpeed;
+    this.weatherDesc = weatherDesc;
+  }
+}
 
 function handleErrors() {
   console.log('error');
@@ -26,27 +39,30 @@ async function defaultApi() {
       windSpeed: weatherData.wind.speed,
       name: weatherData.name,
     };
-    console.log(weatherFinal);
   } catch (err) { handleErrors(); }
 }
 
 defaultApi();
 
+function weatherMake() {
+  const nuWeather = new weatherC();
+  weatherC.name = weatherData.name;
+  weatherC.temp = weatherData.main.temp;
+  weatherC.feelsLike = weatherData.main.feels_like;
+  weatherC.weatherDesc = weatherData.weather[0].description;
+  weatherC.windSpeed = weatherData.wind.windSpeed;
+  console.log(nuWeather);
+}
+
 async function getWeather() {
   try {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchField.value}&APPID=ddc8fb6879ccbfc5d2782d6a632b1b65&units=imperial`, { mode: 'cors' });
     const weatherData = await response.json();
-    const weatherFinal = {
-      temp: weatherData.main.temp,
-      feelsLike: weatherData.main.feels_like,
-      weatherDesc: weatherData.weather[0].description,
-      windSpeed: weatherData.wind.speed,
-    };
-    console.log(weatherFinal);
+    const newWeather = await weatherData.weatherMake();
   } catch (err) { handleErrors(); }
 }
 
 searchB.addEventListener('click', (e) => {
-  e.preventDefault;
+  e.preventDefault();
   getWeather();
 });
